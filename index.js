4b94c534276a471c294e942890c2c97cdea4f46b
tjbigelow <tjbigelow@att.net>
2020-10-26 12:26:05 -0500

    commit tree 9ad9025a12c56973ecf7c448c0c11bffe48aab8d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name_array){
+    var cards = []
+    for (var i = 0; i < name_array.length; i++){
+        cards.push(`Thank you, ${name_array[i]}, for the wonderful surprise gift!`)
+    }
+    return cards
+}
+
+function countDown(num){
+    for (var i = num; i >= 0; i--){
+        console.log(i)
+    }
+}
